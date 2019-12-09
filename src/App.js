3c46c8a69ac5e90f25e79db7af9fa2e8e7b37d7b
Roberto Banbanaste <rbanbanaste@outlook.com>
2019-12-09 17:56:19 -0500

    completed some stretch goals

--- src/App.js
+++ src/App.js
@@ -4,9 +4,30 @@ import "./App.css";
 import BottomRow from "./BottomRow";
 
 function App() {
+  function handleToGoChange(e) {
+    Number.isInteger(parseInt(e.target.value))
+      ? setToGo(e.target.value)
+      : setToGo(0);
+  }
+
+  function handleBallOnChange(e) {
+    Number.isInteger(parseInt(e.target.value))
+      ? setBallOn(e.target.value)
+      : setBallOn(0);
+  }
+
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
   const [homeScore, setHomeScore] = useState(0);
   const [awayScore, setAwayScore] = useState(0);
+  const [quarter, setQuarter] = useState(1);
+  const [down, setDown] = useState(1);
+  const [toGo, setToGo] = useState(0);
+  const [ballOn, setBallOn] = useState(0);
+
+  const inputStyle = {
+    fontSize: "16px",
+    padding: "5px 10px"
+  };
 
   return (
     <div className="container">
@@ -25,11 +46,12 @@ function App() {
             <div className="away__score">{awayScore}</div>
           </div>
         </div>
-        <BottomRow />
+        <BottomRow quarter={quarter} down={down} toGo={toGo} ballOn={ballOn} />
       </section>
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
+
           <button
             className="homeButtons__touchdown"
             onClick={() => setHomeScore(homeScore + 7)}
@@ -56,6 +78,37 @@ function App() {
           >
             Away Field Goal
           </button>
+        </div>
+        {/*  onClick={() => setQuarter(quarter + 1)} */}
+        <div className="homeButtons">
+          <button
+            className="awayButtons__fieldGoal"
+            onClick={() => setQuarter(quarter < 4 ? quarter + 1 : 1)}
+          >
+            Next Quarter
+          </button>
+          <button
+            className="awayButtons__fieldGoal"
+            onClick={() => setDown(down < 4 ? down + 1 : 1)}
+          >
+            Next Down
+          </button>
+        </div>
+        <div className="awayButtons">
+          <input
+            className="awayButtons__fieldGoal"
+            placeholder="Set Yards To Go"
+            type="number"
+            onChange={handleToGoChange}
+            style={inputStyle}
+          ></input>
+          <input
+            className="awayButtons__fieldGoal"
+            placeholder="Set Ball Placement"
+            type="number"
+            onChange={handleBallOnChange}
+            style={inputStyle}
+          ></input>
         </div>
       </section>
     </div>
